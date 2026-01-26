58550af7bb51fd9a4bd7e1cb12a7e30ff3c8f30f
F0rL <kumaloveslife@163.com>
2026-01-26 15:22:47 +0800

    配置环境变量

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  // console.log(env)
+  return {
+    base: env.VITE_BASE_URL,
+    server: {
+      host: '0.0.0.0',
+      port: 5173
+    },
+    define: {},
+    plugins: [vue()]
+  }
 })
